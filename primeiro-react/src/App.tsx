7716d6a8cf1fb35e3c58b73782cae65cb8e91e74
Renata Teixeira <costarenatinha2006@gmail.com>
2026-01-07 21:36:49 -0300

    Aprendendo o que é o Componente React

--- primeiro-react/src/App.tsx
+++ primeiro-react/src/App.tsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
 
-
-export function App() {
-  
-
+//Componente Funcional
+const Test = () => {
   return (
-    <>
-      Olá
-    </>
+    <div>Teste</div>
   )
 }
 
+export function App() {  
+
+  return (
+      <div>
+        Olá
+        <Test></Test> {/*Componente Funcional*/}
+      </div>
+  )
+}
+
+//Função JS
+const teste = () => {
+  return 1 + 1;
+}
+//Função JS
+const useTeste = () => {
+  return 1 + 1;
+}
+//React hook
+const useTest = () => {
+  const [valor] = useState(1+1);
+  return valor;
+}
+//Componente Funcional
+const Testes = () => {
+  return (
+    <div>Teste</div>
+  )
+}
+//Função JS que retorna HTML react
+const test = () => {
+  return (
+    <div>Teste</div>
+  )
+}
